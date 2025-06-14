e7fee1b006fe6fca9ba1c611dc87957196fa2852
sisantaChhatoi <sisantachhatoi@gmail.com>
2025-06-14 17:38:31 +0530

    adding authentication endpoint

--- customers/urls.py
+++ customers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet
+from .views import CustomerViewSet, AuthView
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('authenticate/', AuthView.as_view(), name='authenticate'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from .models import Customer
-from .serializers import CustomerSerializer
+from .serializers import CustomerSerializer, AuthSerializer, UserSerializer
 from .filters import CustomerFilter
 
 # Create your views here.
@@ -15,3 +17,16 @@ class CustomerViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'head']
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = CustomerFilter
+
+class AuthView(APIView):
+    """
+    API endpoint for user authentication.
+    Accepts username, email, and password and returns the user object if authentication is successful.
+    """
+    def post(self, request):
+        serializer = AuthSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
